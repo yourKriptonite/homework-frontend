3040f46fd050e655afc162f5834e0d25f6a08109
yourKriptonite <tko.vladimir@gmail.com>
2019-09-02 14:04:41 +0300

    added requested changes and more test cases

--- source/euclid.js
+++ source/euclid.js
@@ -1,17 +1,47 @@
 'use strict';
 
-
-function gcd(a, b) {
+/**
+ * Calculates gcd of two numbers
+ * @example
+ * // returns 5
+ * globalThis.gcd(5, 10);
+ * @example
+ * // returns 7
+ * globalThis.gcd(21, 14);
+ * 
+ * @param  {number} a - First number must be an integer.
+ * @param {number} b - Second number must be an integer.
+ * @return {number} Return value
+ */
+globalThis.gcd = (a, b) => {
     if (b === 0) {
         return a;
     }
+
     const residue = a % b;
 
-    return gcd(b, residue)
+    return globalThis.gcd(b, residue)
 }
 
 
-function euclid(...numbers) {
+/**
+ * Calculates gcd of any number of args
+ * @example
+ * // returns 5
+ * globalThis.euclid(5, 10, 15);
+ * @example
+ * // returns 4
+ * globalThis.euclid(4, 16, 8, 12);
+ * @example
+ * // returns undefined
+ * globalThis.euclid("String", 16, 8, "Hello");
+ * 
+ * @param {...number} numbers - The function accepts a variable 
+ *      number of numeric parameters.
+ * @return {number} Return value
+ * @return {undefined} Return value if contracts not obrserved
+ */
+globalThis.euclid = (...numbers) => {
     if (!numbers.length) {
         return;
     }
@@ -22,9 +52,7 @@ function euclid(...numbers) {
         }
     }
 
-    const result = numbers.reduce((previous, next) => {
-        return gcd(previous, next)}
-    );
+    const result = numbers.reduce((previous, next) =>  globalThis.gcd(previous, next));
 
     return result;
 }
